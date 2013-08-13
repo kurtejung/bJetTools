c5ccc6773be22e6f407bb5bc01dc0ce7bab882d4
mandrenguyen <matthew.nguyen@cern.ch>
2013-08-13 22:04:23 +0200

    added gluon splitting to systematics plot

--- plotSystematicComponents.C
+++ plotSystematicComponents.C
@@ -16,14 +16,16 @@ TH1F *transformReco2Gen(TH1F *h, TH2F *hXform){
     }
     genErrMC = sqrt(genErrMC);
     htemp->SetBinContent(i+1, genMC);
-    htemp->SetBinError(i+1, genErrMC);
+    //htemp->SetBinError(i+1, genErrMC);
   }
   return htemp;
 }
 
+TH1F *zeroErrors(TH1F *h){
+  for(int i=0;i<h->GetNbinsX();i++) h->SetBinError(i+1,0);
+}
 
-
-void plotSystematicComponents(bool doTransform=1, bool ppPbPb=1, bool plotSymmetrized=0)
+void plotSystematicComponents(bool doTransform=1, bool ppPbPb=1, bool plotSymmetrized=1)
 {
 
   gStyle->SetErrorX(0);
@@ -42,7 +44,7 @@ void plotSystematicComponents(bool doTransform=1, bool ppPbPb=1, bool plotSymmet
     }
   }
 
-  TFile *f0, *f1a, *f1b, *f2, *f3;
+  TFile *f0, *f1a, *f1b, *f2, *f3, *f4a, *f4b;
 
   if(ppPbPb){
     // Default parameters and LTJP systematic
@@ -54,17 +56,19 @@ void plotSystematicComponents(bool doTransform=1, bool ppPbPb=1, bool plotSymmet
     f2 = new TFile("outputTowardsFinal/bFractionDataTemplate_ppPbPb1_SSVHEat2.0FixCL1_bin_0_40_eta_0_2_binomErrors_jet55_wideBin_v2.root");
     // fix charm
     f3 = new TFile("outputTowardsFinal/bFractionMCTemplate_ppPbPb1_SSVHEat2.0FixCL1_bin_0_40_eta_0_2_binomErrors_jet55_wideBin_v2.root");
+    f4a = new TFile("outputTowardsFinal/bFractionMCTemplate_gspUp_ppPbPb1_SSVHEat2.0FixCL0_bin_0_40_eta_0_2_binomErrors_jet55_wideBin_v2.root");
+    f4b = new TFile("outputTowardsFinal/bFractionMCTemplate_gspDown_ppPbPb1_SSVHEat2.0FixCL0_bin_0_40_eta_0_2_binomErrors_jet55_wideBin_v2.root");
   }
   else{
     // Default parameters and LTJP systematic
-    TFile *f0 = new TFile("output/NewFormatV5_bFractionMCTemplate_pppp1_SSVHEat2.0FixCL0_bin_0_40_eta_0_2.root");
+    f0 = new TFile("output/NewFormatV5_bFractionMCTemplate_pppp1_SSVHEat2.0FixCL0_bin_0_40_eta_0_2.root");
     // Working point variation
-    TFile *f1a = new TFile("output/NewFormatV5_bFractionMCTemplate_pppp1_SSVHEat2.5FixCL0_bin_0_40_eta_0_2.root");
-    TFile *f1b = new TFile("output/NewFormatV5_bFractionMCTemplate_pppp1_SSVHEat1.8FixCL0_bin_0_40_eta_0_2.root");
+    f1a = new TFile("output/NewFormatV5_bFractionMCTemplate_pppp1_SSVHEat2.5FixCL0_bin_0_40_eta_0_2.root");
+    f1b = new TFile("output/NewFormatV5_bFractionMCTemplate_pppp1_SSVHEat1.8FixCL0_bin_0_40_eta_0_2.root");
     // Data-driven template
-    TFile *f2 = new TFile("output/NewFormatV5_bFractionDataTemplate_pppp1_SSVHEat2.0FixCL1_bin_0_40_eta_0_2.root");
+    f2 = new TFile("output/NewFormatV5_bFractionDataTemplate_pppp1_SSVHEat2.0FixCL1_bin_0_40_eta_0_2.root");
     // fix charm
-    TFile *f3 = new TFile("output/NewFormatV5_bFractionMCTemplate_pppp1_SSVHEat2.0FixCL1_bin_0_40_eta_0_2.root");
+    f3 = new TFile("output/NewFormatV5_bFractionMCTemplate_pppp1_SSVHEat2.0FixCL1_bin_0_40_eta_0_2.root");
   }
   // Divide by efficiency
 
@@ -118,35 +122,52 @@ void plotSystematicComponents(bool doTransform=1, bool ppPbPb=1, bool plotSymmet
   hCharm->Divide(hEff3);
 
  if(doTransform) hCharm = transformReco2Gen(hCharm, xNorm);
+ 
+ // gluon systematics
+  TH1F *hRawSpec4a =  (TH1F*)f4a->Get("hRawBData");
+  TH1F *hEff4a =  (TH1F*)f4a->Get("hBEfficiencyMC");
+  
+  TH1F *hGlueUp = hRawSpec4a->Clone("hGlueUp");
+  hGlueUp->Divide(hEff4a);
 
-  //plotting style stuff
-  hLTJP->SetMarkerStyle(4);
-  hWorkingPointUp->SetMarkerStyle(4);
-  hWorkingPointDown->SetMarkerStyle(4);
-  hDataDriven->SetMarkerStyle(4);
-  hCharm->SetMarkerStyle(4);
+  TH1F *hRawSpec4b =  (TH1F*)f4b->Get("hRawBData");
+  TH1F *hEff4b =  (TH1F*)f4b->Get("hBEfficiencyMC");
+  
+  TH1F *hGlueDown = hRawSpec4b->Clone("hGlueDown");
+  hGlueDown->Divide(hEff4b);
 
-  hLTJP->SetMarkerColor(2);
-  hWorkingPointUp->SetMarkerColor(3);
-  hWorkingPointDown->SetMarkerColor(4);
-  hDataDriven->SetMarkerColor(6);
-  hCharm->SetMarkerColor(7);
+  if(doTransform) hGlueUp = transformReco2Gen(hGlueUp, xNorm);
+  if(doTransform) hGlueDown = transformReco2Gen(hGlueDown, xNorm);
 
-  hLTJP->SetLineColor(2);
-  hWorkingPointUp->SetLineColor(3);
-  hWorkingPointDown->SetLineColor(4);
-  hDataDriven->SetLineColor(6);
-  hCharm->SetLineColor(7);
+ //plotting style stuff
 
-
-  hDefault->Draw();
-  hLTJP->Draw("same");
-
-  hWorkingPointUp->Draw("same");
-  hWorkingPointDown->Draw("same");
-  hDataDriven->Draw("same");
-  hCharm->Draw("same");
-
+ hLTJP->SetMarkerColor(2);
+ hWorkingPointUp->SetMarkerColor(3);
+ hWorkingPointDown->SetMarkerColor(7);
+ hDataDriven->SetMarkerColor(6);
+ hCharm->SetMarkerColor(4);
+ hGlueUp->SetMarkerColor(kOrange+1);
+ hGlueDown->SetMarkerColor(kOrange+1);
+ 
+ hLTJP->SetLineColor(2);
+ hWorkingPointUp->SetLineColor(3);
+ hWorkingPointDown->SetLineColor(7);
+ hDataDriven->SetLineColor(6);
+ hCharm->SetLineColor(4);
+ hGlueUp->SetLineColor(kOrange+1);
+ hGlueDown->SetLineColor(kOrange+1);
+ 
+ 
+ hDefault->Draw();
+ hLTJP->Draw("same");
+ 
+ hWorkingPointUp->Draw("same");
+ hWorkingPointDown->Draw("same");
+ hDataDriven->Draw("same");
+ hCharm->Draw("same");
+ hGlueUp->Draw("same");
+ hGlueDown->Draw("same");
+ 
 
   // Now take fractional systematics
 
@@ -156,24 +177,29 @@ void plotSystematicComponents(bool doTransform=1, bool ppPbPb=1, bool plotSymmet
   TH1F *hFracLTJP = hLTJP->Clone("hFracLTJP");
   TH1F *hFracWorkingPointUp = hWorkingPointUp->Clone("hFracWorkingPointUp");
   TH1F *hFracWorkingPointDown = hWorkingPointDown->Clone("hFracWorkingPointDown");
-  TH1F *hFracWorkingPoint = hWorkingPointUp->Clone("hFracWorkingPoint");
   TH1F *hFracDataDriven = hDataDriven->Clone("hFracDataDriven");
   TH1F *hFracCharm = hCharm->Clone("hFracCharm");
+  TH1F *hFracGlueUp = hGlueUp->Clone("hFracGlueUp");
+  TH1F *hFracGlueDown = hGlueDown->Clone("hFracGlueDown");
   
   hFracTotal->Reset();
   hFracLTJP->Reset();
   hFracWorkingPointUp->Reset();
   hFracWorkingPointDown->Reset();
-  hFracWorkingPoint->Reset();
   hFracDataDriven->Reset();
   hFracCharm->Reset();
-  
+  hFracGlueUp->Reset();
+  hFracGlueDown->Reset();
 
+  TH1F *hFracWorkingPoint = hWorkingPointUp->Clone("hFracWorkingPoint");
+  TH1F *hFracGlue = hGlueUp->Clone("hFracGlue");
+  
   TH1F *hFracTotalInv = (TH1F*)hFracTotal->Clone("hFracTotalInv");
   TH1F *hFracLTJPInv = (TH1F*)hFracLTJP->Clone("hFracLTJPInv");
   TH1F *hFracWorkingPointInv = (TH1F*)hFracWorkingPoint->Clone("hFracWorkingPointInv");
   TH1F *hFracDataDrivenInv = (TH1F*)hFracDataDriven->Clone("hFracDataDrivenInv");
   TH1F *hFracCharmInv = (TH1F*)hFracCharm->Clone("hFracCharmInv");
+  TH1F *hFracGlueInv = (TH1F*)hFracGlue->Clone("hFracGlueInv");
 
   for(int i =0;i < hDefault->GetNbinsX(); i++){
 
@@ -184,34 +210,42 @@ void plotSystematicComponents(bool doTransform=1, bool ppPbPb=1, bool plotSymmet
     float fracWorkingPointDown = hWorkingPointDown->GetBinContent(i+1)/valDefault-1.;
     float fracDataDriven = hDataDriven->GetBinContent(i+1)/valDefault-1.;
     float fracCharm = hCharm->GetBinContent(i+1)/valDefault-1.;
+    float fracGlueUp = hGlueUp->GetBinContent(i+1)/valDefault-1.;
+    float fracGlueDown = hGlueDown->GetBinContent(i+1)/valDefault-1.;
     
     hFracLTJP->SetBinContent(i+1,fracLTJP);
     hFracWorkingPointUp->SetBinContent(i+1,fracWorkingPointUp);
     hFracWorkingPointDown->SetBinContent(i+1,fracWorkingPointDown);
     hFracDataDriven->SetBinContent(i+1,fracDataDriven);
     hFracCharm->SetBinContent(i+1,fracCharm);
-
-
-    hFracLTJPInv->SetBinContent(i+1,-1.*fracLTJP);
-    hFracDataDrivenInv->SetBinContent(i+1,1.*fracDataDriven);
-    hFracCharmInv->SetBinContent(i+1,-1.*fracCharm);
-
-    
+    hFracGlueUp->SetBinContent(i+1,fracGlueUp);
+    hFracGlueDown->SetBinContent(i+1,fracGlueDown);
 
     float fracWorkingPoint = TMath::Max(fabs(fracWorkingPointUp),fabs(fracWorkingPointDown));
-    
+    float fracGlue = TMath::Max(fabs(fracGlueUp),fabs(fracGlueDown));
+
     hFracWorkingPoint->SetBinContent(i+1,fracWorkingPoint);
+    hFracGlue->SetBinContent(i+1,fracGlue);
+
+    hFracLTJPInv->SetBinContent(i+1,-1.*fracLTJP);
     hFracWorkingPointInv->SetBinContent(i+1,-1.*fracWorkingPoint);
+    hFracDataDrivenInv->SetBinContent(i+1,-1.*fracDataDriven);
+    hFracCharmInv->SetBinContent(i+1,-1.*fracCharm);
+    hFracGlueInv->SetBinContent(i+1,-1.*fracGlue);
+        
 
     float fracTotal = sqrt(fracLTJP*fracLTJP+
 			   fracWorkingPoint*fracWorkingPoint+
 			   fracDataDriven*fracDataDriven+
-			   fracCharm*fracCharm
+			   fracCharm*fracCharm+
+			   fracGlue*fracGlue
 			   );
     
     hFracTotal->SetBinContent(i+1,fracTotal);
     hFracTotalInv->SetBinContent(i+1,-1.*fracTotal);
-    
+
+    // Don't need errors
+    /*
     float effDefault = hDefault->GetBinError(i+1);
     float relErrDefault = effDefault/valDefault;
 
@@ -233,7 +267,7 @@ void plotSystematicComponents(bool doTransform=1, bool ppPbPb=1, bool plotSymmet
     hFracWorkingPointDown->SetBinError(i+1,errFracWorkingPointDown);
     hFracDataDriven->SetBinError(i+1,errFracDataDriven);
     hFracCharm->SetBinError(i+1,errFracCharm);
-    
+    */
     
 
 
@@ -256,17 +290,21 @@ void plotSystematicComponents(bool doTransform=1, bool ppPbPb=1, bool plotSymmet
 
 
   hFracWorkingPointUp->SetFillColor(hFracWorkingPointUp->GetMarkerColor());
-  hFracWorkingPointUp->SetFillStyle(3017);
+  hFracWorkingPointUp->SetFillStyle(3006);
   if(!plotSymmetrized)hFracWorkingPointUp->Draw("h,same");
   hFracWorkingPointDown->SetFillColor(hFracWorkingPointDown->GetMarkerColor());
-  hFracWorkingPointDown->SetFillStyle(3013);
+  hFracWorkingPointDown->SetFillStyle(3006);
   if(!plotSymmetrized)hFracWorkingPointDown->Draw("h,same");
 
+  hFracWorkingPoint = zeroErrors(hFracWorkingPoint);
+  hFracWorkingPointInv = zeroErrors(hFracWorkingPointInv);
+
+  hFracWorkingPoint = zeroErrors(hFracWorkingPoint);
   hFracWorkingPoint->SetFillColor(hFracWorkingPoint->GetMarkerColor());
-  hFracWorkingPoint->SetFillStyle(3017);
+  hFracWorkingPoint->SetFillStyle(3007);
   if(plotSymmetrized)hFracWorkingPoint->Draw("h,same");
   hFracWorkingPointInv->SetFillColor(hFracWorkingPoint->GetMarkerColor());
-  hFracWorkingPointInv->SetFillStyle(3017);
+  hFracWorkingPointInv->SetFillStyle(3007);
   if(plotSymmetrized)hFracWorkingPointInv->Draw("h,same");
 
 
@@ -285,21 +323,33 @@ void plotSystematicComponents(bool doTransform=1, bool ppPbPb=1, bool plotSymmet
   hFracCharmInv->SetFillColor(hFracCharm->GetMarkerColor());
   hFracCharmInv->SetFillStyle(3005);
   if(plotSymmetrized)hFracCharmInv->Draw("h,same");
+  
+  hFracGlue = zeroErrors(hFracGlue);
+  hFracGlueInv = zeroErrors(hFracGlueInv);
+
+  hFracGlue->SetFillColor(hFracGlue->GetMarkerColor());
+  hFracGlue->SetFillStyle(3007);
+  hFracGlue->Draw("h,same");
+  hFracGlueInv->SetFillColor(hFracGlue->GetMarkerColor());
+  hFracGlueInv->SetFillStyle(3007);
+  if(plotSymmetrized)hFracGlueInv->Draw("h,same");
 
   hFracTotal->Draw("h,same");
   hFracTotalInv->Draw("h,same");
 
-
+  
 
   TLegend *leg=new TLegend(0.5,0.7,0.85,0.95);
   leg->SetBorderSize(0);
   leg->SetFillStyle(0);
+  leg->AddEntry(hFracTotal,"Total","l");
   leg->AddEntry(hFracLTJP,"Lifetime Tagger","f");
   if(!plotSymmetrized)leg->AddEntry(hFracWorkingPointUp,"Working point up","f");
   if(plotSymmetrized)leg->AddEntry(hFracWorkingPoint,"Working point","f");
   if(!plotSymmetrized)leg->AddEntry(hFracWorkingPointDown,"Working point down","f");
   leg->AddEntry(hFracDataDriven,"Data-driven template","f");
   leg->AddEntry(hFracCharm,"Charm","f");
+  leg->AddEntry(hFracGlue,"Glue","f");
   leg->Draw();
 
   
